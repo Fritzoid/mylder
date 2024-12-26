f146bfbbb88126ac63c19044ef19df930c28ce8d
Frederic Gos <frederic.gos@gmail.com>
2024-12-26 23:40:58 +0100

    Issue 2: Add road segment

--- src/settlement.rs
+++ src/settlement.rs
@@ -1,9 +1,39 @@
-use crate::roads::Road;
+use crate::roads::RoadSegment;
+use bevy::prelude::*;
 
-struct Settlement {
-    name: String,
-    location: (f32, f32),
-    population: u32,
-    roads: Vec<Road>,
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Component)]
+struct Location(f32, f32);
+
+#[derive(Component)]
+struct Roads(Vec<RoadSegment>);
+
+#[derive(Bundle)]
+pub struct Settlement {
+    name: Name,
+    location: Location,
+    roads: Roads,
 }
 
+impl Settlement {
+    pub fn new(name: String, location: (f32, f32)) -> Self {
+        Self {
+            name: Name(name),
+            location: Location(location.0, location.1),
+            roads: Roads(vec![RoadSegment::default()]),
+        }
+    }
+
+    pub fn spawn(
+        &self,
+        commands: &mut Commands,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<StandardMaterial>>,
+    ) {
+        for rs in &self.roads.0 {
+            rs.spawn(commands, meshes, materials);
+        }
+    }
+}
